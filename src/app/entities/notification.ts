6d1905a02e84af40af7e80f50e056926b7e4b107
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-14 15:42:27 -0300

    Added new updates in the Notification

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -11,11 +11,39 @@ export class Notification {
 
     constructor(props: NotificationProps) { this.props = props }
 
+    public set recipientId(recipientId: string) {
+        this.props.recipientId = recipientId;
+    }
+
+    public get recipientId(): string {
+        return this.props.recipientId;
+    }
+
     public set content(content: string) {
         this.props.content = content;
     }
 
     public get content(): string {
         return this.props.content;
+    }
+
+    public set category(category: string) {
+        this.props.category = category;
+    }
+
+    public get category(): string {
+        return this.props.category;
+    }
+
+    public set readAt(readAt: Date | null | undefined) {
+        this.props.readAt = readAt;
+    }
+
+    public get readAt(): Date | null | undefined {
+        return this.props.readAt;
+    }
+
+    public get createdAt(): Date {
+        return this.props.createdAt;
     }
 }
