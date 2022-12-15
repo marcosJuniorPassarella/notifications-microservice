5f431d72f5bc710c47761e9bf70ecfc3c6183ce1
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-15 10:27:45 -0300

    Added send notification test

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,24 +1,17 @@
-import { Notification } from "../entities/notification";
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-    async create(notification: Notification) {
-        notifications.push(notification);
-    }
-}
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications';
 
 describe('Send notification', () => {
 
     it('should be able to send a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
         const { notification } = await sendNotification.execute({
             content: 'This is a notification',
             category: 'social',
             recipientId: 'example-recipient-id'
         })
-        expect(notification).toBeTruthy();
-        expect(notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
     })
 })
