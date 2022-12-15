55f64a65efed12c220eed74f54b5d1ef741476f4
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-15 13:28:35 -0300

    Added injectable decorator

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
 import { Notification } from "src/app/entities/notification";
 import { NotificationsRepository } from "../../../../app/repositories/notifications-repository"
 import { PrismaService } from '../prisma.service';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
 
     constructor(private prismaService: PrismaService) { }
