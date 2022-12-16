940213f9934087861649ec1127265b4fc8188875
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-16 19:05:08 -0300

    Added save method

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -46,7 +46,13 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     }
 
     async save(notification: Notification): Promise<void> {
-        throw new Error("Method not implemented.");
+        const raw = PrismaNotificationMapper.toPrisma(notification);
+        await this.prisma.notification.update({
+            where: {
+                id: raw.id,
+            },
+            data: raw,
+        });
     }
 
 }
