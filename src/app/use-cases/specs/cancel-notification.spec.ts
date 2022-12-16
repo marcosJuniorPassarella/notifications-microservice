49e9d6936d07eae96a758a6a65f7fdb334fa7a34
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-16 16:10:13 -0300

    Added cancel-notifications unitary tests

---
+++ src/app/use-cases/specs/cancel-notification.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications';
+import { CancelNotification } from '../cancel-notification';
+import { Notification } from "../../entities/notification"
+import { Content } from '@app/entities/content';
+import { NotificationNotFound } from '../errors/notification-not-found';
+
+describe('Cancel notification', () => {
+
+    it('should be able to cancel a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        const notification = new Notification({
+            category: 'social',
+            content: new Content('Nova solicitação de amizade!'),
+            recipientId: 'example-recipient-id'
+        });
+
+        await notificationsRepository.create(notification);
+        await cancelNotification.execute({
+            notificationId: notification?.id
+        });
+
+        expect(notificationsRepository.notifications[0]?.canceledAt).toEqual(
+            expect.any(Date)
+        );
+    });
+
+    it('should not be able to cancel a non existing notification', () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
+        const cancelNotification = new CancelNotification(notificationsRepository);
+
+        expect(() => {
+            return cancelNotification.execute({
+                notificationId: 'fake-notification-id'
+            });
+        }).rejects.toThrow(NotificationNotFound);
+    });
+
+});
