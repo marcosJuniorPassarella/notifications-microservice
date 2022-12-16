518ee360ee3aae9ef8d94cd4a0e2c17311ecda6f
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-16 19:08:00 -0300

    Added findManyByRecipientById method

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -24,7 +24,13 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
     }
 
     async findManyByRecipientById(recipientId: string): Promise<Notification[]> {
-        throw new Error("Method not implemented.");
+        const notifications = await this.prisma.notification.findMany({
+            where: {
+                recipientId,
+            },
+        });
+
+        return notifications.map(PrismaNotificationMapper.toDomain)
     }
 
     async countManyByRecipientById(recipientId: string): Promise<number> {
