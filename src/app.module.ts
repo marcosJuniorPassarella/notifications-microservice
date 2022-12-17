cbfdf6ec24be4ed49b996eea39337db3a704a078
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2022-12-17 15:42:39 -0300

    Addd more kafka configs in the main.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MessaginModule } from './infra/messaging/messaging.module';
 import { HttpModule } from './infra/http/http.module';
 import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule, MessaginModule],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices/interfaces';
+import { KafkaConsumerService } from './infra/messaging/kafka/kafka-consumer.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const kafkaConsumerService = app.get(KafkaConsumerService);
   app.useGlobalPipes(new ValidationPipe())
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService
+  });
   await app.listen(3000);
 }
 bootstrap();
